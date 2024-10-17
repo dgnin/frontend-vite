bcac580043a332ccd2fa729105e6d035028186e2
dgnin <davidgomeznin@protonmail.com>
2024-10-17 10:56:11 +0200

    Configs unit testing

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import { Config } from 'jest';
+
+const config: Config = {
+  testEnvironment: 'jsdom',
+  transform: { '^.+\\.(ts|tsx)$': 'ts-jest' },
+  testRegex: '<rootDir>/tests/.*\\.test\\.(ts|tsx)$',
+  moduleNameMapper: {
+    '^.*\\.scss$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['<rootDir>/tests/setupTests.ts'],
+};
+export default config;
